5d5fa66922cc5358120a445a9a10fb5c8d7ab440
sen-mao <senmaonk@gmail.com>
2025-04-22 03:24:32 +0800

    update

--- README.md
+++ README.md
@@ -9,8 +9,45 @@
 Above a certain threshold of steps, such as 15 steps in SD2.1, the model maintains image generation quality (Fig.a-b) while the features show high similarity (Fig.c). Below this threshold, feature similarity deteriorates along with worse generation quality, accompanied by a degradation in image generation quality as sampling steps reduce. Furthermore, the encoder features consistently exhibit higher similarity than the decoder across all sampling steps (Fig.c). Additionally, the decoder feature always shows much higher variations than encoder (Fig.c).
 
 
-Hence we use a novel design with 1-step encoder and a 4-step decoder, achieving near 1-step inference. Since the 4-step decoder captures richer semantics, ours aligns the generation quality with multi-step DMs.
+Hance we use a novel design with 1-step encoder and a 4-step decoder, achieving near 1-step inference. Since the 4-step decoder captures richer semantics, ours aligns the generation quality with multi-step DMs.
 
-# TODO
-- [ ] Release inference code and weight
+### Update
+- **2025.04.22**: Release the pre-trained models for [Loopfree SD1.5](https://huggingface.co/senmaonk/loopfree-sd1.5) and [Loopfree SD2.1-Base](https://huggingface.co/senmaonk/loopfree-sd2.1-base), and inference code. 😀
+- **2025.03.15**: This repo is created.
+
+### TODO
+- [x] Release inference code and weight
 - [ ] Release training code
+
+---
+
+### Dependencies and Installation
+
+```
+# git clone this repository
+git clone https://github.com/sen-mao/Loopfree.git
+cd Loopfree
+
+# create new anaconda env
+conda create -n loopfree python=3.8 -y
+conda activate loopfree
+
+# install python dependencies
+pip3 install -r requirements.txt
+```
+
+### Inference:
+
+```
+# Loopfree SD1.5
+python loopfree.py --pretrained_model_name_or_path stable-diffusion-v1-5/stable-diffusion-v1-5 \
+                   --student_pretrained_model_name_or_path senmaonk/loopfree-sd1.5/unet \
+                   --output_dir loopfree-sd1.5 --use_parallel
+```
+
+```
+# Loopfree SD2.1-Base
+python loopfree.py --pretrained_model_name_or_path stabilityai/stable-diffusion-2-1-base \
+                   --student_pretrained_model_name_or_path senmaonk/loopfree-sd2.1-base \
+                   --output_dir loopfree-sd2.1-base --use_parallel
+```
---
+++ loopfree.py
@@ -0,0 +1,151 @@
+import os
+import numpy as np
+from PIL import Image
+import argparse
+import torch
+from diffusers import DDIMScheduler, DiffusionPipeline, UNet2DConditionModel, AutoencoderKL
+
+from utils.se_parallel_utils import register_se_forward  # share encoder
+
+
+# Adapted from pipelines.StableDiffusionPipeline.encode_prompt
+@torch.no_grad()
+def encode_prompt(pipe, prompt):
+    tokenizer = pipe.tokenizer
+    text_encoder = pipe.text_encoder
+
+    captions = [prompt]
+    with torch.no_grad():
+        text_inputs = tokenizer(
+            captions,
+            padding="max_length",
+            max_length=tokenizer.model_max_length,
+            truncation=True,
+            return_tensors="pt",
+        )
+        text_input_ids = text_inputs.input_ids
+        prompt_embeds = text_encoder(
+            text_input_ids.to(text_encoder.device),
+        )[0]
+
+    return {"prompt_embeds": prompt_embeds.cpu()}
+
+
+@torch.no_grad()
+def inference(vae, unet, scheduler, encoded_embeds, generator, device, weight_dtype, register_store):
+    input_shape = (1, 4, 64, 64)
+    input_noise = torch.randn(input_shape, generator=generator, device=device, dtype=weight_dtype)
+
+    prompt_embed = encoded_embeds["prompt_embeds"]
+    prompt_embed = prompt_embed.to(device, weight_dtype)
+
+    pred_original_sample = predict_original(unet, scheduler, input_noise, prompt_embed, register_store)
+    pred_original_sample = pred_original_sample / vae.config.scaling_factor
+
+    image = vae.decode(pred_original_sample.to(dtype=vae.dtype)).sample.float()
+    image = (image[0].detach().cpu() * 127.5 + 128).clamp(0, 255).to(torch.uint8)
+
+    return image
+
+def predict_original(unet, scheduler, latents, prompt_embeds, register_store):
+    noises_pred = []
+    for i, t in enumerate(scheduler.timesteps):
+        register_store['se_step'] = i
+        noise_pred = unet(latents, t, encoder_hidden_states=prompt_embeds)['sample']
+
+        if register_store['use_parallel']:
+            break
+
+        noises_pred += [noise_pred]
+
+        # compute the previous noisy sample x_t -> x_t-1
+        latents = scheduler.step(noise_pred, t, latents).prev_sample
+
+    if register_store['use_parallel']:
+        batch_size = latents.shape[0]
+        for i, t in enumerate(register_store['timesteps']):
+            curr_noise = noise_pred[i * batch_size: (i + 1) * batch_size]
+            noises_pred += [curr_noise]
+            latents = scheduler.step(curr_noise, t, latents).prev_sample
+
+    return latents
+
+
+def main(args):
+    device, dtype = "cuda", torch.float16
+
+    unet = UNet2DConditionModel.from_pretrained(args.student_pretrained_model_name_or_path, torch_dtype=dtype)
+    vae = AutoencoderKL.from_pretrained(args.pretrained_model_name_or_path, subfolder="vae", torch_dtype=torch.float32)
+    scheduler = DDIMScheduler.from_pretrained(args.pretrained_model_name_or_path, subfolder="scheduler")
+    pipe = DiffusionPipeline.from_pretrained(args.pretrained_model_name_or_path, scheduler=scheduler, torch_dtype=dtype,
+                                             unet=unet)
+    vae = vae.to(device)
+    pipe = pipe.to(device)
+
+    register_store = {'se_step': None, 'skip_feature': None, 'mid_feature': None, 'lora_scale': None,
+                      'use_parallel': False, 'timesteps': [], 'bs': 1}
+    register_store['use_parallel'] = args.use_parallel
+    register_se_forward(pipe.unet, register_store)
+
+    ## Loopfree need ddim scheduler
+    scheduler.set_timesteps(args.num_inference_steps)
+    if register_store['use_parallel']:
+        register_store['timesteps'] = scheduler.timesteps
+
+    with open(args.captions_file, 'r', encoding='utf-8') as f:
+        prompts = f.readlines()
+    output_dir = args.output_dir
+    os.makedirs(output_dir, exist_ok=True)
+
+    for i, prompt in enumerate(prompts):
+        generator = (torch.Generator(device=device).manual_seed(args.seed) if args.seed else None)
+        encoded_embeds = encode_prompt(pipe, prompt)
+        image = inference(vae, pipe.unet, scheduler, encoded_embeds, generator, device, dtype, register_store)
+        image_array = np.asarray(image).astype(np.uint8)
+        image = Image.fromarray(np.transpose(image_array, (1, 2, 0)))
+        image.save(f"{output_dir}/{i:05}.png")
+    
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Simple example of an inference script.")
+    parser.add_argument(
+        "--pretrained_model_name_or_path",
+        type=str,
+        default=None,
+        required=True,
+        help="Path to pretrained model or model identifier from huggingface.co/models.",
+    )
+    parser.add_argument(
+        "--student_pretrained_model_name_or_path",
+        type=str,
+        default=None,
+        required=True,
+        help="Path to pretrained model or model identifier from huggingface.co/models.",
+    )
+    parser.add_argument(
+        "--captions_file",
+        type=str,
+        default="captions.txt",
+        required=False,
+        help="Path to the captions file.",
+    )
+    parser.add_argument(
+        "--seed",
+        type=int,
+        default=None,
+        required=False,
+        help="Random seed used for inference.",
+    )
+    parser.add_argument(
+        "--output_dir",
+        type=str,
+        default=None,
+        required=True,
+        help="Random seed used for inference.",
+    )
+
+    parser.add_argument('--use_parallel', action="store_true", help="if use parallel encoder share for training")
+    parser.add_argument('--num_inference_steps', type=int, default=4, help="num_inference_steps for share encoder")
+
+    args = parser.parse_args()
+    main(args)
